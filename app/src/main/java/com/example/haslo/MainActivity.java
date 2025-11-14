260b5645b936cc6ed2f41dab661272eec37df2ee
BarlomiejKrol4429 <bartlomiej.krol@uczen.mechaniktg.pl>
2025-11-14 10:26:58 +0100

    Dodano przycisk i sprawdzanie poprawności emailu/hasła

--- app/src/main/java/com/example/haslo/MainActivity.java
+++ app/src/main/java/com/example/haslo/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.haslo;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +27,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText email = findViewById(R.id.email);
+        EditText haslo = findViewById(R.id.haslo);
+        EditText haslo2 = findViewById(R.id.haslo2);
+        Button sprawdz = findViewById(R.id.sprawdz);
+
+        sprawdz.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String email_s = String.valueOf(email.getText());
+                        String pass = String.valueOf(haslo.getText());
+                        String pass_confirm = String.valueOf(haslo2.getText());
+
+                        if (!email_s.contains("@")){
+                            Toast.makeText(MainActivity.this, "Niepoprawny email!", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            if(!pass.equals(pass_confirm)){
+                                Toast.makeText(MainActivity.this, "Niepoprawne hasło!", Toast.LENGTH_SHORT).show();
+                            }else{
+
+                            }
+                        }
+                    }
+                }
+        );
     }
 }
